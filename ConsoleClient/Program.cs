d291022f092ec93dd944407f9bc914cb173a8560
thefenry <thefenry@gmail.com>
2014-09-18 16:45:39 -0700

    Created an xml client to test the api request

---
+++ ConsoleClient/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Net.Http;
+using System.Xml.Linq;
+
+namespace ConsoleClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new HttpClient();
+            client.DefaultRequestHeaders.Accept.Add(
+                new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/xml"));
+
+            var result = client.GetAsync(new Uri("http://localhost:50205/api/videos")).Result;
+            if (result.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                var doc = XDocument.Load(result.Content.ReadAsStreamAsync().Result);
+                var ns = (XNamespace)"http://schemas:datacontract.org/2004/07/Videos.Models";
+
+                foreach (var title in doc.Descendants(ns + "Title"))
+                {
+                    Console.WriteLine(title.Value);
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
