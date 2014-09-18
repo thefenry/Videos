0049fd2265f411cc26699f493aed002df53816d9
thefenry <thefenry@gmail.com>
2014-09-18 15:57:31 -0700

    implementing put method
    This code implements the put method that is used to update the
    individual movie attribute/components

--- Videos/Controllers/VideosController.cs
+++ Videos/Controllers/VideosController.cs
@@ -5,6 +5,8 @@ using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 using Videos.Models;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
 
 namespace Videos.Controllers
 {
@@ -25,19 +27,45 @@ namespace Videos.Controllers
         }
 
         // GET: api/Video/5
-        public string Get(int id)
+        public Video Get(int id)
         {
-            return "value " + id.ToString();
+            var video = db.Videos.Find(id);
+            
+            if (video == null)
+            {
+                throw new HttpResponseException(Request.CreateResponse(HttpStatusCode.NotFound));
+            }
+
+            return video;
         }
 
         // POST: api/Video
-        public void Post([FromBody]string value)
+        public string Post(string value)
         {
+            return value;
         }
 
         // PUT: api/Video/5
-        public void Put(int id, [FromBody]string value)
+        public HttpResponseMessage PutVideo(int id, Video video)
         {
+            if (ModelState.IsValid && id==video.Id)
+            {
+                db.Entry(video).State = EntityState.Modified;
+
+                try
+                {
+                    db.SaveChanges();
+                    return Request.CreateResponse(HttpStatusCode.OK, video);
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    return Request.CreateResponse(HttpStatusCode.NotFound);
+                }
+            }
+            else
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest);
+            }
         }
 
         // DELETE: api/Video/5
